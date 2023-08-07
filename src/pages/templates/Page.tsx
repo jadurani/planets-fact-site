12734cb4a6888f6fa602fc0a7938aca716d07cfb
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-07 13:26:12 +0800

    refactor(storybook): placed templates in their own directories

--- src/pages/templates/Page.tsx
+++ src/pages/templates/Page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { Header } from "./Header";
+// import { Header } from "@/components/templates/header/Header";
+import { Header } from "../../components/templates/header/Header";
 import "./page.css";
 
 type User = {
