6827dbe48ce8fdba1a9a791a8b80b1cf8ecdad90
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-07 08:04:22 +0800

    build(storybook): set preview breakpoints

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,47 @@
 import type { Preview } from "@storybook/react";
 
+const BREAKPOINTS_INT = {
+  xs: {
+    width: 320,
+    height: 568,
+  },
+  sm: {
+    width: 640,
+    height: 360,
+  },
+  md: {
+    width: 768,
+    height: 432,
+  },
+  lg: {
+    width: 1024,
+    height: 576,
+  },
+  xl: {
+    width: 1280,
+    height: 720,
+  },
+  "2xl": {
+    width: 1536,
+    height: 864,
+  },
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val["width"]}px`,
+          height: `${val["height"]}px`,
+        },
+      },
+    ];
+  })
+);
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,6 +51,7 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    viewport: { viewports: customViewports },
   },
 };
 
