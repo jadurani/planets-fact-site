77f82b5bb452e3311a8d3eb7580b84ed17fdae8e
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-15 00:01:52 +0800

    chore(tailwind): added project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#070724",
+      grey1: "#38384F",
+      grey2: "#838391",
+      "grey-hover": "D8D8D8",
+      mercury: "#419EBB",
+      venus: "#EDA249",
+      earth: "#6f2ed6",
+      mars: "#D14C32",
+      jupiter: "#D83A34",
+      saturn: "#CD5120",
+      uranus: "#1ec2a4",
+      neptune: "#2d68f0",
+    },
     screens: {
       xs: "320px",
       sm: "640px",
