6cf37db6b1cc4ea2a5d29aab94e86f0eddef7fce
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-17 11:09:20 +0800

    feat: added top border on nav items

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -13,7 +13,7 @@ export const NavBar = () => {
         {PLANETS.map((planet, idx) => (
           <li
             key={idx}
-            className="p-4 uppercase font-spartan font-bold tracking-wider">
+            className={`p-4 uppercase font-spartan font-bold tracking-wider border-${planet} border-t-4 border-solid`}>
             {planet}
           </li>
         ))}
