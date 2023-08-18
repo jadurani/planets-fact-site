6b5aedc659b7326f2f1f6de814d48c664ee34cf7
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-18 22:08:14 +0800

    feat: prelim page

---
+++ src/pages/[planet]/[profile].page.tsx
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router";
+
+export default function ProfilePage() {
+  const router = useRouter();
+  return <div>ProfilePage</div>;
+}
---
+++ src/pages/[planet]/index.page.tsx
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router";
+
+export default function PlanetPage() {
+  const router = useRouter();
+  return <div>{router.pathname}</div>;
+}
