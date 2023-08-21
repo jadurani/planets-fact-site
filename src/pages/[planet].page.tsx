4ecfa5960b5b530448341c3f3628297b1aecc0b8
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-21 15:02:46 +0800

    feat: layouts and routing (#3)
    - Update layouts and implement routing
    - Added empty route handler
    - Updated header tag
    
    ![PR3](https://github.com/jadurani/planets-fact-site/assets/11599005/4465828b-372a-4371-ae89-e688c256006e)
    
    ![image](https://github.com/jadurani/planets-fact-site/assets/11599005/e9c17679-b0e6-48cd-b241-1a89d05a9c1a)

--- src/pages/[planet].page.tsx
+++ src/pages/[planet].page.tsx
@@ -4,6 +4,7 @@ import { Profile } from "@/components/profile/Profile";
 import { DESCRIPTIONS, DescriptionType } from "@/components/tablink/TabLink";
 import { TabNav } from "@/components/tabnav/TabNav";
 import { Planet } from "@/lib/planets.constant";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 export default function PlanetPage() {
@@ -15,33 +16,56 @@ export default function PlanetPage() {
     desc = "overview";
   }
 
+  const planetTitle = planet?.toUpperCase();
+
   return (
-    <div className="container mx-auto">
-      {/* profile image */}
-      <Profile planet={planet} desc={desc} />
-
-      {/* description */}
-      <div>
-        <h1 className="text-center md:text-start font-antonio uppercase text-h2 md:text-[48px] lg:text-h1">
-          {planet}
-        </h1>
-
-        <Description
-          content="Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth."
-          source="https://en.wikipedia.org/wiki/Mercury_(planet)"
+    <>
+      <Head>
+        <title>{planetTitle} Planet Facts </title>
+        <meta
+          name="description"
+          content="Designed by FE Mentor; Implemented by Jadurani Davalos"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="md:hidden">
+        <TabNav planet={planet} desc={desc} />
       </div>
 
-      {/* subnav */}
-      <TabNav planet={planet} desc={desc} />
+      <div className="container flex flex-col m-auto py-8 px-4 justify-center h-full">
+        <div className="grid grid-cols-1 lg:grid-cols-3">
+          {/* profile image */}
+          <div className="lg:col-span-2 md:p-20 flex items-center mx-auto justify-center m-8 w-40 h-40 md:w-[500px] md:h-[500px]">
+            <Profile planet={planet} desc={desc} />
+          </div>
 
-      {/* facts */}
-      <div className="flex flex-col md:grid grid-cols-4 gap-4">
-        <Fact property="rotation" value="58.6 Days" />
-        <Fact property="revolution" value="87.97 Days" />
-        <Fact property="radius" value="2,439.7 KM" />
-        <Fact property="temperature" value="430°c" />
+          {/* description */}
+          <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-8 my-auto items-center">
+            <div>
+              <h1 className="text-center md:text-start font-antonio uppercase text-h2 md:text-[48px] lg:text-h1">
+                {planet}
+              </h1>
+
+              <Description
+                content="Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth."
+                source="https://en.wikipedia.org/wiki/Mercury_(planet)"
+              />
+            </div>
+            <div className="hidden md:block">
+              <TabNav planet={planet} desc={desc} />
+            </div>
+          </div>
+        </div>
+
+        {/* facts */}
+        <div className="flex flex-col md:grid grid-cols-4 gap-4 lg:my-16">
+          <Fact property="rotation" value="58.6 Days" />
+          <Fact property="revolution" value="87.97 Days" />
+          <Fact property="radius" value="2,439.7 KM" />
+          <Fact property="temperature" value="430°c" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
