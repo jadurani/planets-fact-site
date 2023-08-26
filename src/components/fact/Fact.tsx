6c615166d18d4a609f2ae4f1c69e9ea3b6007c60
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-08-26 21:04:26 +0800

    fix: removed h6 - fixed warning about heading levels should only increase by one

--- src/components/fact/Fact.tsx
+++ src/components/fact/Fact.tsx
@@ -8,9 +8,9 @@ export const Fact = ({ property, value }: FactProps) => (
     className="uppercase border border-white/20 px-5 py-3 md:py-4
      flex md:inline-flex md:flex-col md:gap-1 items-center md:items-start justify-between
      object-cover">
-    <h6 className="font-spartan font-medium tracking-widest text-white/50 text-[8px]">
+    <div className="font-spartan font-medium tracking-widest text-white/50 text-[8px]">
       {property}
-    </h6>
+    </div>
     <span className="font-antonio font-light text-white text-[20px] md:text-[24px]">
       {value}
     </span>
